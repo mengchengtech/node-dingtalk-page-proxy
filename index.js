43f0605acd8bc768944c64d428cdec8e025fa114
DC <greenlee713@126.com>
2020-05-21 20:21:08 +0800

    删除冗余  修改:合并两种target处理

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ const app = new Koa()
 const staticPath = './static'
 
 const router = new Router()
-router.get('/', (ctx, next) => {
-  ctx.body = 'Hello world'
-})
-router.get('/redirect', (ctx, next) => {
-  ctx.redirect('http://www.163.com')
-})
-
-router.get('/ins', async (ctx, next) => {
-  await ctx.render('post')
-})
 
 router.get('/auth', async (ctx, next) => {
   if (!ctx.query.accessToken) {
@@ -50,9 +40,6 @@ app
   .use(static(path.join(__dirname, staticPath)))
   .use(router.routes())
   .use(router.allowedMethods())
-  .use(ctx => {
-    ctx.body = ctx.body || 'Hello whatever'
-  })
   .listen(3000, () => {
     console.log('3000')
   })
--- static/page-index.js
+++ static/page-index.js
@@ -1,15 +1,23 @@
 // import * as dd from 'dingtalk-jsapi'
 
-window.onload = ddGo()
+window.onload = checkTarget()
 
-async function ddGo () {
+function checkTarget () {
+  const url = new URL(window.location.href)
+  const searchParams = new URLSearchParams(url.search)
+  ddGo(searchParams.get('agentId'), searchParams.get('type'))
+}
+
+async function ddGo (agentId, type) {
+  console.log('config', globalConfig)
   try {
     const codeRes = await getDDCode()
     if (codeRes && codeRes.code) {
       console.log('code', codeRes)
-      replaceToCas(codeRes.code)
-      showFailDialog()
+      replaceToTarget(type, agentId, codeRes.code)
+      return
     }
+    showFailDialog()
   } catch (error) {
     console.log('get code error', error)
   }
@@ -20,7 +28,7 @@ async function getDDCode () {
     dd.ready(async function () {
       // dd.ready参数为回调函数，在环境准备就绪时触发，jsapi的调用需要保证在该回调函数触发后调用，否则无效。
       dd.runtime.permission.requestAuthCode({
-        corpId: 'ding188c13dcf749c6ac35c2f4657eb6378f',
+        corpId: globalConfig.cropId,
         onSuccess: function (result) {
           console.log('res', result)
           resolve(result)
@@ -33,9 +41,13 @@ async function getDDCode () {
   })
 }
 
-function replaceToCas (code) {
+function replaceToTarget (type, agentId, code) {
+  const url =
+    (type && type === 'invoker'
+      ? globalConfig.invokerTarget
+      : globalConfig.casTarget) + `?agentId=${agentId}&code=${code}`
   dd.biz.navigation.replace({
-    url: `http://dev.mctech.vip/cas/third/private/dingTalk/page-login?agentId=10003&code=${code}`,
+    url,
     onSuccess: () => {
       console.log('replaced')
     },
@@ -57,7 +69,7 @@ function showFailDialog () {
         dd.biz.navigation.close()
       }
       if (index === 1) {
-        ddGo()
+        checkTarget()
       }
     },
     onFail (err) {
